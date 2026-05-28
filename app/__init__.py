449667dec461f4d1aae7bd463000a0f09eb377ef
waimea-bbridgeman <brbridgeman@waimea.school.nz>
2026-05-29 09:56:03 +1200

    message editing

--- app/__init__.py
+++ app/__init__.py
@@ -220,20 +220,49 @@ def delete(id):
         return redirect("/messages")
         
 #-----------------------------------------------------------
-# Edit 
+# Edit message
 #-----------------------------------------------------------
 @app.get("/message/edit/<int:id>")
 @login_required
 def edit(id):
     with connect_db() as db: 
         sql = """
-            SELECT FROM 
-            messages
-            WHERE id=?
+            SELECT 
+                messages.id     AS mid, 
+                messages.title, 
+                messages.body,
+                users.id        AS uid,
+                users.forename
+
+            FROM messages
+            JOIN users ON messages.user_id = users.id
+            WHERE messages.id=? 
     """
         params = [id]
+        message = db.execute(sql, params).fetchone() 
+        return render_template("pages/message_edit.jinja", message=message) 
+
+#-----------------------------------------------------------
+# Post Edit 
+#-----------------------------------------------------------
+@app.post("/message/edit/<int:id>")
+@login_required
+def edit_post(id):
+    title = request.form.get("title", "").strip()
+    body  = request.form.get("body", "").strip()
+
+    with connect_db() as db:
+        sql = """
+            UPDATE messages
+            SET title=?, body=?
+            WHERE id=?
+        """
+        params = [title, body, id]
+
         db.execute(sql, params)
-        return redirect("/messages")   
+
+        flash("Message updated", "success")
+    return redirect("/messages")
 #-----------------------------------------------------------
 # Help page - Show some help
 #-----------------------------------------------------------
